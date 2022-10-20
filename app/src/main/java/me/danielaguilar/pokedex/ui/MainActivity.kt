63a3ce77d9129dc98d060d26d8d7e5ba94d29c5b
Daniel <daguilar.loncomilla@gmail.com>
2022-10-20 11:43:22 -0300

    @daniel00k Initial setup

---
+++ app/src/main/java/me/danielaguilar/pokedex/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package me.danielaguilar.pokedex.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import me.danielaguilar.pokedex.R
+
+import me.danielaguilar.pokedex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostContainer)
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.pokedexIndexFragment,
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
